38c71303bbeeff5ea67628b12a7105ce7a48c035
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-17 22:04:31 +0530

    First java program Testing

---
+++ Java/Basics/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args){
+        // hamara sara code yahna hoga main block
+        System.out.println("\n Sabyasachhi Routray");          //printing statement
+    }
+}
