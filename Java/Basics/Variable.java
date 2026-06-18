449dbcb7c76da587f735087823c068c7c4941b97
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-18 12:24:15 +0530

    Variable and naming covention

---
+++ Java/Basics/Variable.java
@@ -0,0 +1,32 @@
+public class Variable {
+    public static void main(String[] args) {
+        // variable naming covention
+
+        /* Variable is a container that hold valid  specific type values */
+
+
+        // below declaration and intialization at a time
+        int x = 5;  // x vale container main 5 value assign kardo
+        System.out.println(x);  // here x considered as conainer and print the value which is stored int he container named x
+        System.out.println("x"); // here x treated as string
+
+        int y; //decalaration y is local variable to main()
+        y = 20; // initialization
+
+        x = 15; 
+        // variable update ho raha hai
+
+        System.out.println(x + "\t" + y); 
+        // Here + act as concatinate
+
+
+
+
+        // this is single line comment
+
+        /*This 
+        is 
+        multi 
+        line comment */
+    }
+}
