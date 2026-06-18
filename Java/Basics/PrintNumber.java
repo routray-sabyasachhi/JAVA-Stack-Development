009ff459e6b9f7dcf0f244755c39b7fb6193d5a3
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-18 11:53:09 +0530

    Test to number printing

---
+++ Java/Basics/PrintNumber.java
@@ -0,0 +1,6 @@
+public class PrintNumber {
+    public static void main(String[] args) {
+        System.out.println(33+1);     // treat as number to 33+1 = 34
+        System.out.println("33+1"); // "33+1" consider as string so print as it is in o/p
+    }
+}
