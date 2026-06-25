02e7c12efd5824f93289629f88e9fe2ae03e83b2
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-25 13:45:06 +0530

    Finding Largest element among all elements

---
+++ Java/Array/FindLargestElement.java
@@ -0,0 +1,21 @@
+package Java.Array;
+
+public class FindLargestElement {
+    public static void main(String[] args) {
+        int[] arr = {1,5,8,85,47,69,-35,-5,-18};
+
+        int large = arrayLargeElement(arr, arr.length);
+        System.out.println("Array's large Element : " + large);
+    }
+
+    private static int arrayLargeElement(int[] arr, int n) {
+
+        int large = arr[0]; // here consider array element as largest
+
+        for(int i = 1; i<n; i++) {
+            if(arr[i] > large)
+                large = arr[i];
+        }
+        return large;
+    }
+}
