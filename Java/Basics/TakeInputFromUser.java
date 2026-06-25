1351e12d5ed138b5624d6cf92fb7fbb04d91b272
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-25 15:01:55 +0530

    Take input from user with resourse management

---
+++ Java/Basics/TakeInputFromUser.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class TakeInputFromUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int userInput = sc.nextInt();
+        sc.close();   // resourse management
+        System.out.println("Entered Input is : " + userInput);
+    }
+}
