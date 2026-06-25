32fa39119564a81a0af9e917939167e36ebb1805
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-25 14:34:10 +0530

    Remove duplicate -> Approach 2

---
+++ Java/Array/RemoveDuplicate2.java
@@ -0,0 +1,26 @@
+package Java.Array;
+
+
+import java.util.Arrays;
+
+public class RemoveDuplicate2 {
+    public static void main(String[] args) {
+
+        int[] arr = {1,5,-18,8,85,47,5,69,-35,85,-5,-18};
+
+        Arrays.sort(arr);
+
+        int j = 0;
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        System.out.println(
+            Arrays.toString(Arrays.copyOf(arr, j + 1))
+        );
+    }
+}
