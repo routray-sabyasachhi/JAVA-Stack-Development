40dccdb6c9d8f94bcfcab12337488783a6d601eb
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-25 13:49:13 +0530

    Finding Smallest Element in an Array

---
+++ Java/Array/FindSmallestElement.java
@@ -0,0 +1,21 @@
+package Java.Array;
+
+public class FindSmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {1,5,8,85,47,69,-35,-5,-18};
+
+        int small = arraySmallElement(arr, arr.length);
+        System.out.println("Array's smallest Element : " + small);
+    }
+
+    private static int arraySmallElement(int[] arr, int n) {
+
+        int small = arr[0]; // here consider array element as largest
+
+        for(int i = 1; i<n; i++) {
+            if(arr[i] < small)
+             small = arr[i];
+        }
+        return small;
+    }
+}
