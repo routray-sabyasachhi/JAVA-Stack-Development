862640b1fa256739728051cfaa741119d828085b
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-25 13:31:59 +0530

    Sum of Array Elements

---
+++ Java/Array/SumArrayElement.java
@@ -0,0 +1,18 @@
+package Java.Array;
+
+public class SumArrayElement {
+    public static void main(String[] args) {
+        int[] arr = {1,5,8,85,47,69,-35,-5,-18};
+
+        int sum = sumArray(arr, arr.length);
+        System.out.println("Sum of Array Elements : " + sum);
+    }
+
+    public static int sumArray(int[] arr, int n) {
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
