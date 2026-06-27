fe8b0d2ca70ff465052e875f103a39cbf7ae3317
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-27 20:49:07 +0530

    Bubble sort function building

--- Java/Array/Array_Sort/BubbleSort.java
+++ Java/Array/Array_Sort/BubbleSort.java
@@ -1,7 +1,39 @@
-package Java.Array.Array_Sort;
+//package Java.Array.Array_Sort;
 
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {1,5,8,85,47,69,-35,-5,-18};
+
+        System.out.println("Before sorting: ");
+        for(int num : arr)
+            System.out.print(num + " ");
+
+        // Sorting function call
+
+        bubbleSort(arr);
+
+
+
+        System.out.println();
+
+        System.out.println("After Sorting :");
+        for(int num : arr)
+            System.out.print(num + " ");
     }
+
+    public static void bubbleSort(int[] arr) {
+    int size = arr.length;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 }
