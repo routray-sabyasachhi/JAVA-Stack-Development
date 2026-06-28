813ff8c36d6b667a7bba3b874696409738a4315f
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-06-28 20:14:04 +0530

    Insertion Sort

--- Java/Array/Array_Sort/InsertionSort.java
+++ Java/Array/Array_Sort/InsertionSort.java
@@ -3,5 +3,33 @@ package Java.Array.Array_Sort;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {1,5,8,85,47,69,-35,-5,-18};
+
+        System.out.println("Brfore sorting :");
+
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        insertionSort(arr);
+
+        System.out.println("After sorting : ");
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSort(int[] arr) {
+        int size = arr.length;
+        for(int i=1; i<size; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j >= 0 && (arr[j]>key)){
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
     }
 }
