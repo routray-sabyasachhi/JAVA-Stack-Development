9742993ec9fbf53979c2d09b33b06240c919284b
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-07-03 21:45:29 +0530

    Reverse the array by two pointer method

--- Java/Array/Basics/ReverseAnArray.java
+++ Java/Array/Basics/ReverseAnArray.java
@@ -2,6 +2,33 @@ package Java.Array.Basics;
 
 public class ReverseAnArray {
     public static void main(String[] args) {
-        int[] arr = {1,5,8,85,47,69,-35,-5,-18};
+        int[] arr = {1,5,8,85,47,69,-35,-5,-18};  // Initial array
+
+        System.out.println("Before array reverse :");
+        for(int data : arr){
+            System.out.print(data + " ");
+        }
+
+        reverseArray(arr);
+
+        System.out.println("\n After array reverse :");
+        for(int data : arr){
+            System.out.print(data + " ");
+        }
+    }
+
+    public static void reverseArray(int[] arr) {
+        // solve this by two pointer method
+
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
     }
 }
