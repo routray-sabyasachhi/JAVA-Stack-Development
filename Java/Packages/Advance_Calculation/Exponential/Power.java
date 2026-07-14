0bc8d1892634c53346c4d1aee2b015ce08036b72
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-07-14 21:01:55 +0530

    Added my pow method and access form other file Main.java

---
+++ Java/Packages/Advance_Calculation/Exponential/Power.java
@@ -0,0 +1,7 @@
+package Java.Packages.Advance_Calculation.Exponential;
+
+public class Power {
+    public static void myPower(double x, double y){
+        System.out.println(Math.abs(Math.pow(x, y)));
+    }
+}
