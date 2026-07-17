576cf01899b37f80407f9b41fc4224e7e908e459
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-07-17 15:13:47 +0530

    Checking

---
+++ Java/FileHandling/CharacterEncodingDemo.java
@@ -0,0 +1,9 @@
+package Java.FileHandling;
+
+import java.nio.charset.StandardCharsets;
+
+public class CharacterEncodingDemo {
+    public static void main(String[] args) {
+        System.out.println(StandardCharsets.UTF_8.name());
+    }
+}
