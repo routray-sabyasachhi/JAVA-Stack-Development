cd187e1dc00f1ee9af81bf1bb7c83a8fc8ee13f8
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-08-14 21:30:57 +0530

    Linked Hash Set

---
+++ Java/Collection_Frameworks/LinkedHashSetInterface.java
@@ -0,0 +1,22 @@
+package Java.Collection_Frameworks;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetInterface {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        set.add(5);
+        set.add(10);
+        set.add(15);
+        set.add(20);
+        System.out.println("LinkedHashSet: " + set);
+
+        set.remove(set.toArray()[1]);
+        System.out.println("After removing element at index 1: " + set);
+
+        set.add(85);
+        System.out.println("After adding 85: " + set);
+        set.hashCode();
+        System.out.println("Hash code of the LinkedHashSet: " + set.hashCode());
+    }
+}
