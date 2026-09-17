60d3409aa073bfc36bcf95fbce6dbb7fba83718f
routray-sabyasachhi <sabyasachhi.me@gmail.com>
2026-09-17 22:06:36 +0530

    create singly linked list

---
+++ DSA/LinkedList/Singly-LinkedList/CreateNewList.java
@@ -0,0 +1,33 @@
+public class CreateNewList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void printList() {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        CreateNewList list = new CreateNewList();
+        list.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        list.head.next = second;
+        second.next = third;
+
+        list.printList();
+    }
+}
